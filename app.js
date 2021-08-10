406fc6e17431aca946aa86f337753845a7c16a6f
sibasis malla <sibasisml@gmail.com>
2021-08-10 23:27:01 +0530

    sessionBug restored 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet')
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const dbUrl = process.env.DB_URL||'mongodb://localhost:27017/yelp-camp'
 //mongodb://localhost:27017/yelp-camp
 const secret = process.env.SECRET || 'garbage'
@@ -93,7 +93,9 @@ app.listen(port,()=>{
 })
 const store = MongoStore.create({
     mongoUrl:dbUrl,
-    secret,
+    crypto:{
+        secret
+    },
     touchAfter: 24*3600
 })
 store.on('error',function(e){
